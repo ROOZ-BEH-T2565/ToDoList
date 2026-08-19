624e4a00249370eccc0218e00a7a38e675ccdf75
ROOZ-BEH-T2565 <roozbeh.ppc4@gmail.com>
2026-08-19 18:17:34 +0330

    Add today's date to the header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import style from "./Header.module.css";
 export const Header = () => {
+  const getDate = new Date();
+  const todayDate = getDate.toLocaleDateString("fa-IR", {
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <header className={style.header}>
       <div className={style.col}>
         <span>menu</span>
-        <span className={style.date}>14 sept</span>
+        <span className={style.date}>{todayDate}</span>
         <span>taghvim</span>
       </div>
       <div className={style.col}>
